4fd2ee276950ad4928c8374ee321efaf5bb027ce
muflax <mail@muflax.com>
2013-10-23 10:48:44 +0100

    #longest method

--- lib/muflax/enumerable.rb
+++ lib/muflax/enumerable.rb
@@ -3,3 +3,22 @@
 # Copyright muflax <mail@muflax.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
+module Enumerable
+  # find shortest element
+  def shortest
+    self.min_by{|s| s.to_s.length}
+  end
+
+  # find longest element
+  def longest
+    self.max_by{|s| s.to_s.length}
+  end
+
+  def length_of_longest
+    self.longest.to_s.length
+  end
+
+  def length_of_shortest
+    self.shortest.to_s.length
+  end
+end
