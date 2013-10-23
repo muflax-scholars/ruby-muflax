11d2e6d77f51dadc6421fefe2191a97cc4a3b189
muflax <mail@muflax.com>
2013-10-23 10:48:57 +0100

    starts_with? alias

---
+++ lib/muflax/string.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+# Copyright muflax <mail@muflax.com>, 2013
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+class String
+  alias :starts_with? :start_with?
+  alias :ends_with?   :end_with?
+end
