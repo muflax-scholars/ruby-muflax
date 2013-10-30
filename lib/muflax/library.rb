26634109555aa6149c7e7bc0e03c46dbb2c38e02
muflax <mail@muflax.com>
2013-10-30 21:49:29 +0000

    also work around __FILE__ issue for require_local

--- lib/muflax/library.rb
+++ lib/muflax/library.rb
@@ -3,8 +3,8 @@
 # Copyright muflax <mail@muflax.com>, 2013
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-def require_local lib_name
-  file = File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__
+def require_local lib_name, location=__FILE__
+  file = File.symlink?(location) ? File.readlink(location) : location
   lib = File.join File.dirname(file), "/../lib/#{lib_name}"
 
   if File.exists? lib
--- muflax.gemspec
+++ muflax.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "muflax"
-  s.version = "0.1.1"
+  s.version = "0.1.2"
 
   s.authors = ["muflax"]
   s.summary = "muflax standard library"
