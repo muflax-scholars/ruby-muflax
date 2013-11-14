497dd0e6328a08823535acd7c74b94a385109b1f
muflax <mail@muflax.com>
2013-11-14 04:47:07 +0000

    shortcut for binding.pry

---
+++ lib/muflax/debug.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+# Copyright muflax <mail@muflax.com>, 2013
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+def bp
+  binding.pry
+end
+
+def bpe
+  binding.pry
+  exit
+end
--- muflax.gemspec
+++ muflax.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "muflax"
-  s.version = "0.1.6"
+  s.version = "0.1.7"
 
   s.authors = ["muflax"]
   s.summary = "muflax standard library"
