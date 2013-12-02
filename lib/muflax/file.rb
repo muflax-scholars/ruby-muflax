db7af43dc255156da94a873c4483671a2210e247
muflax <mail@muflax.com>
2013-12-02 22:37:19 +0000

    shortcut for saving in common file formats, currently just Anki-compatible CSV

--- lib/muflax/file.rb
+++ lib/muflax/file.rb
@@ -4,12 +4,21 @@
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
 class File
-  def self.save name, &blk
+  def self.save name, as: :text, &blk
     # make sure directory exists
     dir = File.dirname(name)
     FileUtils.mkdir_p dir if not Dir.exists? dir
 
     # now open it
-    File.open(name, "w", &blk)
+    case as
+    when :anki
+      f = CSV.open(name, "w", :col_sep => "\t", &blk)
+    when :text
+      f = File.open(name, "w", &blk)
+    else
+      raise "unsupported file format: #{as}"
+    end
+
+    f
   end
 end
