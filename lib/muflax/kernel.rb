ab7c9aaf9ae672512563b3fdeb9aa17fa1e2a345
muflax <mail@muflax.com>
2014-12-06 17:40:19 +0000

    hr

---
+++ lib/muflax/kernel.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- encoding: utf-8 -*-
+# Copyright muflax <mail@muflax.com>, 2014
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+module Kernel
+  def hr
+    width, height	= HighLine::SystemExtensions.terminal_size
+    linewidth    	= [(width || 0) - 1, 1].max
+
+    puts "-" * (linewidth)
+  end
+end
